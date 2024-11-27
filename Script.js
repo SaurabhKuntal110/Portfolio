f8b88d0a00fd6d331809d029c1a065ec4442c299
Saurabh Kuntal <wintersoldier110@gmail.com>
2024-11-27 09:22:07 +0000

    responsive design finish

--- Script.js
+++ Script.js
@@ -1,13 +1,38 @@
-const tabLinks = document.getElementsByClassName("tabLinks")
-const tabContents = document.getElementsByClassName("tabContents")
+const tabLinks = document.querySelectorAll(".tabLinks")
+const tabContents = document.querySelectorAll(".tabContents")
+const navCloseBtn = document.querySelector(".fa-xmark")
+const navOpenBtn = document.querySelector(".fa-bars")
+const sideMenu = document.querySelector("#sideMenu")
 
-function openTab(tabName){
-    for(let tabLink of tabLinks){
+tabLinks.forEach((tablink)=>{
+    tablink.addEventListener('click',()=>{
+        console.log("button clicked");
         tabLink.classList.remove("activeLink")
-    }
-    for(let tabContent of tabContents){
+    })
+})
+tabContents.forEach((tabContent)=>{
+    tabContent.addEventListener('click',()=>{
         tabContent.classList.remove("activeTab")
-    }
-    event.currentTarget.classList.add("activeLink");
-    document.getElementById(tabName).classList.add("activeTab");
-}
+    })
+})
+event.currentTarget.classList.add("activeLink");
+document.getElementById(tabName).classList.add("activeTab");
+    
+   
+// function openTab(tabName){
+//     for(let tabLink of tabLinks){
+//         tabLink.classList.remove("activeLink")
+//     }
+//     for(let tabContent of tabContents){
+//         tabContent.classList.remove("activeTab")
+//     }
+//     event.currentTarget.classList.add("activeLink");
+//     document.getElementById(tabName).classList.add("activeTab");
+// }
+
+navCloseBtn.addEventListener('click',()=>{
+    sideMenu.style.right = "-200px";
+})
+navOpenBtn.addEventListener('click',()=>{
+    sideMenu.style.right = "0";
+})
