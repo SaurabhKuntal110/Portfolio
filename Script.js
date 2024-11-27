ef758250787d2e9a418ed90aab58684c32845283
Saurabh Kuntal <wintersoldier110@gmail.com>
2024-11-27 12:51:38 +0000

    Add external JS

--- Script.js
+++ Script.js
@@ -4,35 +4,38 @@ const navCloseBtn = document.querySelector(".fa-xmark")
 const navOpenBtn = document.querySelector(".fa-bars")
 const sideMenu = document.querySelector("#sideMenu")
 
-tabLinks.forEach((tablink)=>{
-    tablink.addEventListener('click',()=>{
-        console.log("button clicked");
-        tabLink.classList.remove("activeLink")
-    })
-})
-tabContents.forEach((tabContent)=>{
-    tabContent.addEventListener('click',()=>{
-        tabContent.classList.remove("activeTab")
-    })
-})
-event.currentTarget.classList.add("activeLink");
-document.getElementById(tabName).classList.add("activeTab");
     
    
-// function openTab(tabName){
-//     for(let tabLink of tabLinks){
-//         tabLink.classList.remove("activeLink")
-//     }
-//     for(let tabContent of tabContents){
-//         tabContent.classList.remove("activeTab")
-//     }
-//     event.currentTarget.classList.add("activeLink");
-//     document.getElementById(tabName).classList.add("activeTab");
-// }
+// Function to open a tab
+function openTab(tabName, clickedTab) {
+    // Remove active classes from all links and contents
+    tabLinks.forEach((tabLink) => {
+        tabLink.classList.remove("activeLink");
+    });
+    tabContents.forEach((tabContent) => {
+        tabContent.classList.remove("activeTab");
+    });
 
+    // Add active class to the clicked tab and corresponding content
+    clickedTab.classList.add("activeLink");
+    document.getElementById(tabName).classList.add("activeTab");
+}
+
+// Add event listeners to each tab link
+tabLinks.forEach((tabLink) => {
+    tabLink.addEventListener("click", () => {
+        const tabName = tabLink.getAttribute("data-tab"); // Get the tab name from a custom attribute
+        openTab(tabName, tabLink);
+    });
+});
+
+
+//Add EventListner to close sidemenu
 navCloseBtn.addEventListener('click',()=>{
     sideMenu.style.right = "-200px";
 })
+
+//Add EventListner to open sidemenu
 navOpenBtn.addEventListener('click',()=>{
     sideMenu.style.right = "0";
 })
